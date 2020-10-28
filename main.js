ec2ae4ace0b40d81c786806720ef3785bdd3485e
Sean McPherson <git@seanmcp.com>
2020-10-28 07:15:24 -0400

    feat: add three overlays

--- main.js
+++ main.js
@@ -1,38 +1,39 @@
 (async () => {
-  // NOTE: Test this on Chrome, Safari, & Firefox
-  const overlayEl = document.getElementById("overlay");
-  document.querySelectorAll("[data-overlay]").forEach((node) => {
-    node.addEventListener("click", async () => {
-      if (overlayEl.webkitRequestFullscreen) {
-        overlayEl.webkitRequestFullscreen();
-      } else {
-        await overlayEl.requestFullscreen();
-      }
-    });
-  });
-
-  document
-    .getElementById("exit-fullscreen-button")
-    .addEventListener("click", () => {
-      if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-      } else {
-        document.exitFullscreen();
-      }
+    // NOTE: Test this on Chrome, Safari, & Firefox
+    const overlayEl = document.getElementById("overlay");
+    document.querySelectorAll("[data-overlay]").forEach((node) => {
+        node.addEventListener("click", async () => {
+            overlayEl.dataset.bg = node.dataset.bg
+            if (overlayEl.webkitRequestFullscreen) {
+                overlayEl.webkitRequestFullscreen();
+            } else {
+                await overlayEl.requestFullscreen();
+            }
+        });
     });
 
-  function toggleFullscreen() {
-    if (document.fullscreenElement || document.webkitIsFullScreen) {
-      document.body.dataset.fullscreen = "true";
-    } else {
-      document.body.removeAttribute("data-fullscreen");
+    document
+        .getElementById("exit-fullscreen-button")
+        .addEventListener("click", () => {
+            if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            } else {
+                document.exitFullscreen();
+            }
+        });
+
+    function toggleFullscreen() {
+        if (document.fullscreenElement || document.webkitIsFullScreen) {
+            document.body.dataset.fullscreen = "true";
+        } else {
+            document.body.removeAttribute("data-fullscreen");
+        }
     }
-  }
 
-  window.addEventListener("fullscreenchange", toggleFullscreen);
-  window.addEventListener("webkitfullscreenchange", toggleFullscreen);
+    window.addEventListener("fullscreenchange", toggleFullscreen);
+    window.addEventListener("webkitfullscreenchange", toggleFullscreen);
 
-  // Safari Mobile?
-  window.addEventListener("webkitbeginfullscreen", toggleFullscreen);
-  window.addEventListener("webkitendfullscreen", toggleFullscreen);
+    // Safari Mobile?
+    window.addEventListener("webkitbeginfullscreen", toggleFullscreen);
+    window.addEventListener("webkitendfullscreen", toggleFullscreen);
 })();
