8a132f9971bbbea7b534be6aedb9275710a6d60f
Sean McPherson <git@seanmcp.com>
2020-10-28 21:46:01 -0400

    feat: add patterns, polish

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
 (async () => {
-    // NOTE: Test this on Chrome, Safari, & Firefox
-    const overlayEl = document.getElementById("overlay");
-    document.querySelectorAll("[data-overlay]").forEach((node) => {
-        node.addEventListener("click", async () => {
-            overlayEl.dataset.bg = node.dataset.bg
-            if (overlayEl.webkitRequestFullscreen) {
-                overlayEl.webkitRequestFullscreen();
-            } else {
-                await overlayEl.requestFullscreen();
-            }
-        });
+  const overlayEl = document.getElementById("overlay");
+  document.querySelectorAll("button.overlay").forEach((node) => {
+    node.addEventListener("click", async () => {
+      overlayEl.style.backgroundImage = node.style.backgroundImage;
+      if (overlayEl.webkitRequestFullscreen) {
+        overlayEl.webkitRequestFullscreen();
+      } else {
+        await overlayEl.requestFullscreen();
+      }
+    });
+  });
+
+  document
+    .getElementById("exit-fullscreen-button")
+    .addEventListener("click", () => {
+      if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else {
+        document.exitFullscreen();
+      }
     });
 
-    document
-        .getElementById("exit-fullscreen-button")
-        .addEventListener("click", () => {
-            if (document.webkitExitFullscreen) {
-                document.webkitExitFullscreen();
-            } else {
-                document.exitFullscreen();
-            }
-        });
-
-    function toggleFullscreen() {
-        if (document.fullscreenElement || document.webkitIsFullScreen) {
-            document.body.dataset.fullscreen = "true";
-        } else {
-            document.body.removeAttribute("data-fullscreen");
-        }
+  function toggleFullscreen() {
+    if (document.fullscreenElement || document.webkitIsFullScreen) {
+      document.body.dataset.fullscreen = "true";
+    } else {
+      document.body.removeAttribute("data-fullscreen");
     }
+  }
 
-    window.addEventListener("fullscreenchange", toggleFullscreen);
-    window.addEventListener("webkitfullscreenchange", toggleFullscreen);
+  window.addEventListener("fullscreenchange", toggleFullscreen);
+  window.addEventListener("webkitfullscreenchange", toggleFullscreen);
 
-    // Safari Mobile?
-    window.addEventListener("webkitbeginfullscreen", toggleFullscreen);
-    window.addEventListener("webkitendfullscreen", toggleFullscreen);
+  // Safari Mobile?
+  window.addEventListener("webkitbeginfullscreen", toggleFullscreen);
+  window.addEventListener("webkitendfullscreen", toggleFullscreen);
 })();
